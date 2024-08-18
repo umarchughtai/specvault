9361c760ae449a39445d70641b7f8a7e6f4af500
umarchughtai <114848707+umarchughtai@users.noreply.github.com>
2024-08-18 10:33:05 +0500

    Updated to latest code

--- specvault.sh
+++ specvault.sh
@@ -236,3 +236,4 @@ fi
 #collect_system_specs
 collect_and_send
 #inxi -N | awk -F ': ' '/Device/ {gsub(" driver.*", "", $2); gsub("Network ", "", $2); sub("Device-", "", $2); if (i == 1) print "Device-" ++i ": " $2; else print "Device-" ++i ": " $3}'
+#inxi -M | grep -i 'model' | awk -F': ' '{print $3}'
